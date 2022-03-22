2ab2d638f1cefd25bee740044dd856e3da0e9901
murgeshnb <mnbechetti@gmail.com>
2022-03-22 16:45:59 +0530

    automation engg commit for merging

---
+++ src/test/java/prahlad/PrahladClass1.java
@@ -0,0 +1,12 @@
+package prahlad;
+
+public class PrahladClass1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Dear lead please approve from new branch and merge");
+
+	}
+
+}
